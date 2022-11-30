37a63125ab3a7736a49c17f85356519c0cfc2278
Danira98 <danirsorg@gmail.com>
2022-11-29 23:47:26 -0800

    uploading files to second branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522,-118.2437], 14);
 
 //or
 // Create the map object with a center and zoom level.
@@ -13,11 +13,26 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a circle instead to the map for Los Angeles, California
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// Alternative way of creating a marker
+ L.circleMarker([34.0522, -118.2437],{
+    radius: 100,
+    color:"black",
+    fillColor:'#ffffa1'
+ }).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox://styles/mapbox/dark-v11',
     // tileSize: 512,
     // zoomOffset: -1,
     accessToken: API_KEY
