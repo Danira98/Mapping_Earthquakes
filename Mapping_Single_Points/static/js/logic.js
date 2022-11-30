459c45e53dc49ff10349447c48ff03e1f4e1455f
Danira98 <danirsorg@gmail.com>
2022-11-30 00:27:12 -0800

    Uploading Files

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,41 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522,-118.2437], 14);
+
+//or
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a circle instead to the map for Los Angeles, California
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// Alternative way of creating a marker
+ L.circleMarker([34.0522, -118.2437],{
+    radius: 100,
+    color:"black",
+    fillColor:'#ffffa1'
+ }).addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
